635285e9be74d9bd3baa239931897ed12ecf5207
UA-lan <128175037+UA-lan@users.noreply.github.com>
2023-05-01 22:26:44 +0300

    Update pairs.py.py

--- pairs.py.py
+++ pairs.py.py
@@ -1,9 +1,9 @@
-def Print (_i, _j): 
-    print(_i, "+", _j) 
-     
-nums = [1, 4, 3, 6, 9, 7] 
- 
-for i in nums: 
-    for j in nums: 
-        if i + j == 10: 
-            print(i,j)
+def Print(i, r):
+    print(i, "+", r)
+
+nums = [1, 4, 3, 5, 6, 9, 7]
+
+for i in nums:
+    for r in nums:
+        if i + r == 10:
+            print(i, r)
